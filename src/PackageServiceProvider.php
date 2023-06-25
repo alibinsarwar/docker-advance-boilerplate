3d9bfb396965f50ad6f1593c5f6ac7ea0219542f
Muhammad Ali <alibinsarwar2001@gmail.com>
2023-06-25 22:43:28 +0500

    update dir path

--- src/PackageServiceProvider.php
+++ src/PackageServiceProvider.php
@@ -9,7 +9,7 @@ class PackageServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        (new Filesystem)->copyDirectory(__DIR__.'/vendor/alibinsarwar/stub', base_path('/'));
+        (new Filesystem)->copyDirectory(__DIR__.'/vendor/alibinsarwar/stub', __DIR__);
 
     }
 }
